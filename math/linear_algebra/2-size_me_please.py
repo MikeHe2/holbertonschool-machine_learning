003c7330b179cbe86c4d5f2be787f748b3097377
MikeHe2 <michael.he30@hotmail.com>
2024-12-03 15:31:40 -0400

    Add function to determine the shape of a matrix

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+"""Linear algebra"""
+
+def matrix_shape(matrix):
+
+    """Function that return the shape of a matrix"""
+
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
