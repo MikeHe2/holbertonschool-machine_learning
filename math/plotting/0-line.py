2322f3632411c40ccee46ae1415afb2389ecbece
MikeHe2 <michael.he30@hotmail.com>
2024-12-09 19:45:27 -0400

    Add docstring for line function and module header in 0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+
+"""Plotting"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+
+    """Plotting y as a line graph"""
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
