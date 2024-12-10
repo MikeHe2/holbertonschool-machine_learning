01ecd5cbe136a1bce7f45642bdbfd9b107b6f91e
MikeHe2 <michael.he30@hotmail.com>
2024-12-10 01:11:14 -0400

    Change C-14 plot line style to dotted in 3-two.py

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -18,7 +18,7 @@ def two():
     y2 = np.exp((r / t2) * x)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(x, y1, c='r', label = 'C-14', linestyle="--")
+    plt.plot(x, y1, c='r', label = 'C-14', linestyle=":")
     plt.plot(x, y2, c='g', label = 'Ra-226')
     plt.autoscale(tight=True)
     plt.title('Exponential Decay of Radioactive Elements')
