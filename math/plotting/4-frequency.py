77ea5edbba0321a4a23e237f476a7c5748ef315a
MikeHe2 <michael.he30@hotmail.com>
2024-12-10 15:09:22 -0400

    Add frequency histogram plotting for student scores in 4-frequency.py

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Plotting"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+
+    """Function to plot a histogram of student scores for a project"""
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.hist(student_grades, bins=10, edgecolor='black')
+    plt.title('Project A')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.show()
