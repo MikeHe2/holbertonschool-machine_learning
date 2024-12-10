74c0b5bd40a42cb381887aa870f63b3b88bfd6a7
MikeHe2 <michael.he30@hotmail.com>
2024-12-10 15:28:46 -0400

    Update histogram bins and axis limits for student grades in 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,7 +13,10 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades, bins=10, edgecolor='black')
+    plt.hist(student_grades, bins=range(0, 110, 10), edgecolor='black')
+    plt.xticks(np.arange(0, 110, 10))
+    plt.ylim(0, 30)
+    plt.xlim(0, 100)
     plt.title('Project A')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
